5671facca028a0b639962c7bc32d83f02b3db9d5
“TheKiller1959” <thekiller1959yt@gmail.com>
2022-08-06 19:38:15 -0500

    Update cv download function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import '../assets/styles/home.css';
 import '../assets/styles/background.css';
-import cv from '../assets/docs/cv.pdf';
+import cvEn from '../assets/docs/cv-en.pdf';
+import cvEs from '../assets/docs/cv-es.pdf';
 import { useTheme } from "../context/ThemeProvider";
 import { useTranslation } from "react-i18next";
 
 const Home = () => {
   const { theme } = useTheme();
   const { t } = useTranslation("global");
+  const cvToggle = t("lang") === "en" ? cvEn : cvEs;
 
   return (
     <div name="Inicio" className='home' id='home' style={{ backgroundColor: theme.homeBackground, color: theme.hometextColor, transition: ".2s" }} >
@@ -29,7 +31,7 @@ const Home = () => {
         <h3>{t("description.title")}</h3>
       </div>
       <div>
-        <a href={cv} download target="_blank" rel="noopener noreferrer">
+        <a href={cvToggle} download target="_blank" rel="noopener noreferrer">
           <button className='down-btn'>{t("description.button")} 📃</button>
         </a>
       </div>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './assets/styles/index.css';
 import App from './App';
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
-
 import global_es from './assets/translations/es/global.json';
 import global_en from './assets/translations/en/global.json';
 
